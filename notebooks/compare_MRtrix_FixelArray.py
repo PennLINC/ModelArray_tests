5c8c6dbf40b3294e20870a1c3521c949b7c7dd66
zhao-cy <chenyingz@outlook.com>
2021-11-28 18:15:17 -0500

    update

--- notebooks/compare_MRtrix_FixelArray.py
+++ notebooks/compare_MRtrix_FixelArray.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 import pandas as pd
 from scipy.stats import pearsonr
-from plotnine import ggplot, aes, geom_point
+from plotnine import ggplot, aes, geom_point, geom_text
 
 from confixel.fixels import mif_to_nifti2
 
@@ -33,40 +33,80 @@ f_fixelarray_raw_p = folder_fixelarray + "/lm_model.p.value.mif"
 f_fixelarray_p_fdr = folder_fixelarray + "/lm_model.p.value.fdr.mif"
 f_fixelarray_p_bonferroni = folder_fixelarray + "/lm_model.p.value.bonferroni.mif"
 
-f_mrtrix_uncorrected_p = folder_mrtrix + "/uncorrected_pvalue_F1.mif"
-f_mrtrix_fwe_p = folder_mrtrix + "/fwe_1mpvalue_F1.mif"
+f_mrtrix_p_permu = folder_mrtrix + "/uncorrected_pvalue_F1.mif"
+f_mrtrix_p_permu_fwe = folder_mrtrix + "/fwe_1mpvalue_F1.mif"
 
 _, fixelarray_raw_p = mif_to_nifti2(f_fixelarray_raw_p)
 _, fixelarray_p_fdr = mif_to_nifti2(f_fixelarray_p_fdr)
 _, fixelarray_p_bonferroni = mif_to_nifti2(f_fixelarray_p_bonferroni)
 
-_, mrtrix_uncorrected_p = mif_to_nifti2(f_mrtrix_uncorrected_p)
-mrtrix_uncorrected_p = 1 - mrtrix_uncorrected_p
-_, mrtrix_fwe_p = mif_to_nifti2(f_mrtrix_fwe_p)
-mrtrix_fwe_p = 1 - mrtrix_fwe_p
+_, mrtrix_p_permu = mif_to_nifti2(f_mrtrix_p_permu)
+mrtrix_p_permu = 1 - mrtrix_p_permu
+_, mrtrix_p_permu_fwe = mif_to_nifti2(f_mrtrix_p_permu_fwe)
+mrtrix_p_permu_fwe = 1 - mrtrix_p_permu_fwe
 
-pearsonr(fixelarray_raw_p, mrtrix_uncorrected_p)
-pearsonr(fixelarray_p_fdr, mrtrix_uncorrected_p)
-pearsonr(fixelarray_p_fdr, mrtrix_fwe_p)
 
+# # save to text file:
+# temp_fn = open(f_fixelarray_raw_p.replace(".mif",'.txt'), "w")
+# np.savetxt(temp_fn, fixelarray_raw_p)
+# temp_fn.close()
+#
+# temp_fn = open(f_fixelarray_p_fdr.replace(".mif",'.txt'), "w")
+# np.savetxt(temp_fn, fixelarray_p_fdr)
+# temp_fn.close()
+#
+# temp_fn = open(f_fixelarray_p_bonferroni.replace(".mif",'.txt'), "w")
+# np.savetxt(temp_fn, fixelarray_p_bonferroni)
+# temp_fn.close()
+#
+# temp_fn = open(f_mrtrix_p_permu.replace(".mif",'.txt'), "w")
+# np.savetxt(temp_fn, mrtrix_p_permu)
+# temp_fn.close()
+#
+# temp_fn = open(f_mrtrix_p_permu_fwe.replace(".mif",'.txt'), "w")
+# np.savetxt(temp_fn, mrtrix_p_permu_fwe)
+# temp_fn.close()
+
+
+pearsonr(fixelarray_raw_p, mrtrix_p_permu)
+pearsonr(fixelarray_p_fdr, mrtrix_p_permu)
+pearsonr(fixelarray_p_fdr, mrtrix_p_permu_fwe)
+pearsonr(fixelarray_p_bonferroni, mrtrix_p_permu_fwe)
 
 d = {'fixelarray_raw_p': fixelarray_raw_p,
      'fixelarray_p_fdr': fixelarray_p_fdr,
      'fixelarray_p_bonferroni': fixelarray_p_bonferroni,
-     'mrtrix_uncorrected_p': mrtrix_uncorrected_p,
-     'mrtrix_fwe_p': mrtrix_fwe_p} 
+     'mrtrix_p_permu': mrtrix_p_permu,
+     'mrtrix_p_permu_fwe': mrtrix_p_permu_fwe} 
 
 df = pd.DataFrame(data=d)
 
+
+str_title = "corr=" + str(pearsonr(fixelarray_p_fdr, mrtrix_p_permu)[0])
+
+(
+    ggplot(df)   # What data to use
+    + aes(x="fixelarray_p_fdr", y="mrtrix_p_permu")  # What variable to use
+    +  geom_point()
+    # + ggtitle(str)
+    
+)
+
 (
     ggplot(df)  # What data to use
-    + aes(x="fixelarray_p_fdr", y="mrtrix_uncorrected_p")  # What variable to use
+    + aes(x="fixelarray_raw_p", y="mrtrix_p_permu")  # What variable to use
     + geom_point()
 )
 
 (
     ggplot(df)  # What data to use
-    + aes(x="fixelarray_p_fdr", y="mrtrix_fwe_p")  # What variable to use
+    + aes(x="fixelarray_p_fdr", y="mrtrix_p_permu_fwe")  # What variable to use
+    + geom_point()
+)
+
+(
+    ggplot(df)  # What data to use
+    + aes(x="fixelarray_p_bonferroni", y="mrtrix_p_permu_fwe")  # What variable to use
     + geom_point()
 )
 
