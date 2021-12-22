6611a894582e6494731462d6513a9f3398e154bb
zhao-cy <chenyingz@outlook.com>
2021-12-21 22:00:24 -0500

    clean examples

--- notebooks/example_mif_to_h5.sh
+++ notebooks/example_mif_to_h5.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 which_dataset=$1
 
-fn_py="../FixelDB/inst/python/fixeldb/create_backend/fixels.py"
+fn_py="../confixel/fixels.py"
 # input data:
-if [[ "$which_dataset" == "data_from_Val" ]]
+if [[ "$which_dataset" == "data_forCircleCI_n50" ]]
 then
 	index_file="fixeldata/index.mif"
 	directions_file="fixeldata/directions.mif"
-	cohort_file="test_cohort.csv"
-	output_hdf5="fixels.h5"
-	relative_root="../data/data_forCircleCI_n=2"
-elif [[ "$which_dataset" == "data_forCircleCI_n50" ]]
-then
-	index_file="fixeldata/index.mif"
-	directions_file="fixeldata/directions.mif"
-	cohort_file="test_cohort_n50.csv"
+	cohort_file="n50_cohort.csv"
 	colname_subjid="subject_id"
 	output_hdf5="fixels.h5"
-	relative_root="../data/data_forCircleCI_n50"
+	relative_root="../../data/data_forCircleCI_n50"
 elif [[ "$which_dataset" == "data_forCircleCI_n25x2" ]]
 then
 	index_file="fixeldata/index.mif"
@@ -26,22 +19,6 @@ then
 	colname_subjid="subject_id"
 	output_hdf5="fixels_withSubjID.h5"
 	relative_root="../data/data_forCircleCI_n25x2"
-elif [[ "$which_dataset" == "data_from_Val" ]]
-then
-	#index_file="fixeldata/index.mif"
-	index_file="FixelMask_PopulationTemplate/index.mif"
-	#directions_file="fixeldata/directions.mif"
-	directions_file="FixelMask_PopulationTemplate/directions.mif"
-	cohort_file="cohort_ZAPR01.csv"
-	output_hdf5="fixels_wRealSubjID.h5"
-	relative_root="../data/data_from_Val"
-elif [[ "$which_dataset" == "data_from_Val_28yrAndBelow_only1" ]]
-then
-	index_file="FixelMask_PopulationTemplate/index.mif"
-	directions_file="FixelMask_PopulationTemplate/directions.mif"
-	cohort_file="cohort_ZAPR01_28yrAndBelow_only1.csv"
-	output_hdf5="fixels_28yrAndBelow_only1.h5"
-	relative_root="../data/data_from_Val"
 fi
 
 
