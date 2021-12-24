6d85e6e33a485a54c5086c8c7ee2836f2b6eec4f
zhao-cy <chenyingz@outlook.com>
2021-12-23 23:25:14 -0500

    now save source file names instead of subject ID; update README.md

--- notebooks/example_mif_to_h5.sh
+++ notebooks/example_mif_to_h5.sh
@@ -5,18 +5,17 @@ fn_py="../confixel/fixels.py"
 # input data:
 if [[ "$which_dataset" == "data_forCircleCI_n50" ]]
 then
-	index_file="fixeldata/index.mif"
-	directions_file="fixeldata/directions.mif"
+	index_file="FD/index.mif"
+	directions_file="FD/directions.mif"
 	cohort_file="n50_cohort.csv"
-	colname_subjid="subject_id"
 	output_hdf5="fixels.h5"
 	relative_root="../../data/data_forCircleCI_n50"
+	
 elif [[ "$which_dataset" == "data_forCircleCI_n25x2" ]]
 then
 	index_file="fixeldata/index.mif"
 	directions_file="fixeldata/directions.mif"
 	cohort_file="n25x2_cohort.csv"
-	colname_subjid="subject_id"
 	output_hdf5="fixels_withSubjID.h5"
 	relative_root="../data/data_forCircleCI_n25x2"
 fi
@@ -27,7 +26,6 @@ cmd="python $fn_py"
 cmd+=" --index-file ${index_file}"
 cmd+=" --directions-file ${directions_file}"
 cmd+=" --cohort-file ${cohort_file}"
-cmd+=" --colname_subjid ${colname_subjid}"
 cmd+=" --output-hdf5 ${output_hdf5}"
 cmd+=" --relative-root ${relative_root}"
 
