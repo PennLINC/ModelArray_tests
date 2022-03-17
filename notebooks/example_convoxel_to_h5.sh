9b17da1731ab4bbd6d0ba1d7e58b4b1e8db8d443
zhao-cy <chenyingz@outlook.com>
2022-03-17 17:07:56 -0400

    update convoxel to h5 for PNC fMRI n-back; minor update in convoxel.py

--- notebooks/example_convoxel_to_h5.sh
+++ notebooks/example_convoxel_to_h5.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# TO PREPARE:
+# phenotypes.csv: besides the scalar_name and source_file columns, you also need to provide source_mask_file column!!
+
 # TO SETUP:
 # git clone <repo>
 # cd ConFixel
@@ -14,10 +17,10 @@
 
 
 # for Kristin's fMRI n-back project, on cubic:
-cmd="convoxel --group-mask-file data_voxel_kristin_nback/n1601_NbackCoverageMask_20170427.nii.gz"
-cmd+=" --cohort-file data_voxel_kristin_nback/XXXX.csv"
-cmd+=" --relative-root /cbica/projects/fixel_db/dropbox"
-cmd+=" --output-hdf5 data_voxel_kristin_nback/pncNback.h5"
+cmd="convoxel --group-mask-file n1601_NbackCoverageMask_20170427.nii.gz"
+cmd+=" --cohort-file pncNback_phenotypes.csv"
+cmd+=" --relative-root /cbica/projects/fixel_db/data/data_voxel_kristin_nback"
+cmd+=" --output-hdf5 pncNback.h5"
 
 
 echo $cmd
