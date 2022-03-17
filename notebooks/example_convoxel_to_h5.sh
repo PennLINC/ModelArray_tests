2579006b85e38ee88ccd800b4afb1581cedfb0d6
zhao-cy <chenyingz@outlook.com>
2022-03-16 20:54:08 -0400

    add ConVoxel example for PNC n-back data

--- notebooks/example_convoxel_to_h5.sh
+++ notebooks/example_convoxel_to_h5.sh
@@ -4,13 +4,21 @@
 # cd ConFixel
 # pip install -e .
 
-# for grmpy project, on cubic:
-nsubj=215
+# # for grmpy project, on cubic:
+# nsubj=215
 
-cmd="convoxel --group-mask-file stats_ModelArray/tpl-MNI152NLin6Asym_res-02_desc-brain_mask.nii.gz"
-cmd+=" --cohort-file stats_ModelArray/GRMPY_convoxel_meanCBF_n${nsubj}.csv"
-cmd+=" --relative-root /cbica/projects/GRMPY/project/curation/testing/"
-cmd+=" --output-hdf5 stats_ModelArray/GRMPY_meanCBF_n${nsubj}_orig.h5"
+# cmd="convoxel --group-mask-file stats_ModelArray/tpl-MNI152NLin6Asym_res-02_desc-brain_mask.nii.gz"
+# cmd+=" --cohort-file stats_ModelArray/GRMPY_convoxel_meanCBF_n${nsubj}.csv"
+# cmd+=" --relative-root /cbica/projects/GRMPY/project/curation/testing/"
+# cmd+=" --output-hdf5 stats_ModelArray/GRMPY_meanCBF_n${nsubj}_orig.h5"
+
+
+# for Kristin's fMRI n-back project, on cubic:
+cmd="convoxel --group-mask-file data_voxel_kristin_nback/n1601_NbackCoverageMask_20170427.nii.gz"
+cmd+=" --cohort-file data_voxel_kristin_nback/XXXX.csv"
+cmd+=" --relative-root /cbica/projects/fixel_db/dropbox"
+cmd+=" --output-hdf5 data_voxel_kristin_nback/pncNback.h5"
+
 
 echo $cmd
 $cmd
