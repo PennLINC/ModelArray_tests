84b49b4000eddbc6b2d489109e122292ddace388
zhao-cy <chenyingz@outlook.com>
2022-03-18 16:21:18 -0400

    increase h_vmem

--- pncNback_fsl_flameo_wrapper.sh
+++ pncNback_fsl_flameo_wrapper.sh
@@ -2,7 +2,7 @@
 # this is the wrapper.sh for submitting script to cubic compute node
 # configurations were based on what Kristin used: h_vmem=20G, s_vmem=25G
 
-h_vmem="20G"
+h_vmem="30G"
 s_vmem="25G"
 #cmd="qsub -l h_vmem=${h_vmem} s_vmem=${s_vmem} pncNback_fsl_flameo.sh"  # however there is no option of s_vmem???
 cmd="qsub -l h_vmem=${h_vmem} pncNback_fsl_flameo.sh"
