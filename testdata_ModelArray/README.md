348dd9c33d00ddcbfb3fafbd093593a0f2d66226
Chenying Zhao <chenyzh@seas.upenn.edu>
2022-06-28 21:29:06 -0400

    updates on expected values

--- testdata_ModelArray/README.md
+++ testdata_ModelArray/README.md
@@ -1,11 +1,19 @@
 # Generate data for CircleCI tests
-## generate significant data + its expected statistical results w/o using ModelArray
+## generate significant data
 This is to test that the ModelArray's results are as expected. We simulated significant results to do so.
 
 * generate significant data and inject into .h5 file:
     * [generate_data_significant.Rmd](generate_data_significant.Rmd)
-* calculate expected statistical results but without using ModelArray:
+* NOT TO BE USED ANYMORE: calculate expected statistical results but without using ModelArray:
     * [expected_results.Rmd](expected_results.Rmd)
+    * ^^^ has been moved to `ModelArray` repo, which will be calculated within the `testthat` and circle ci (i.e., when testing, first, generate the expected results; then, compare ModelArray's results with the expected results, using the same environment and dependent packages versions).
+* Some notes on the reproducibility of `mgcv::gam` stat results:
+    * Found discrepancy (but relatively small) between expected values calculate locally vs ModelArray results by circle ci, mostly in smooth term's stat or p.value
+    * However the values can be fully replicated by running another time locally - so it's reliable given the same environment
+    * For more: 
+        * see slack on 6/14/22
+        * see https://github.com/PennLINC/ModelArray/issues/65
+
 
 ## data for testing NaN etc:
 
--- testdata_ModelArray/temp_gam_expectedValues.R
+++ testdata_ModelArray/temp_gam_expectedValues.R
@@ -2,8 +2,10 @@
 # can be used both locally and when ssh into circle ci
 
 # load all necessary stuff:
+rm(list=ls())
 devtools::load_all()
 options(digits=15)
+#set.seed(1234)     # DO NOT RUN MULTIPLE TIMES!
 
 ### input data: ####
 h5_path <- system.file("extdata", "n50_fixels.h5", package = "ModelArray")
@@ -29,7 +31,8 @@ load(fn.expected.results)  # variable name: expected.results
 
 
 ### for this case:
-formula <- FD ~ s(age, fx=TRUE) + sex   
+#formula <- FD ~ s(age, fx=TRUE) + sex   
+formula <- FD ~ ti(age, fx=TRUE) + ti(factorB, fx=TRUE) + ti(age, factorB, fx=TRUE) + factorA
 #formula <- FD ~ oSex + s(age,k=4, fx=TRUE) + s(age, by=oSex, fx=TRUE) + factorB 
 
 ### directly call mgcv, instead of using ModelArray: #####
@@ -44,4 +47,22 @@ onemodel.summary <- summary(onemodel)
 onemodel.summary
 onemodel.summary$s.table   # the table for the smooth term such as age
 
+# # re.test = FALSE does not change the results:
+# set.seed(1)
+# onemodel.summary.retestFALSE <- summary(onemodel, re.test=FALSE)
+# onemodel.summary.retestFALSE$s.table
+
 # this means that the mgcv::gam() results are already different!
+
+
+### use ModelArray to calculate:
+#element.subset <- idx.fixel.gam %>% as.integer()
+element.subset <- 1:20
+results.run1 <- ModelArray.gam(formula, data = modelarray, phenotypes = phenotypes, scalar = scalar_name, element.subset = element.subset,
+                                               n_cores = 4, pbar = FALSE, full.outputs = TRUE)
+results.run2 <- ModelArray.gam(formula, data = modelarray, phenotypes = phenotypes, scalar = scalar_name, element.subset = element.subset,
+                               n_cores = 4, pbar = FALSE, full.outputs = TRUE)
+testthat::expect_equal(results.run1, 
+                       results.run2)
+testthat::expect_identical(results.run1, 
+                       results.run2)
